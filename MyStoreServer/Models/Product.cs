7f4a995a2cfa90ed646999ee53d5a765698d17c1
chmilkka <mishachmil228335@gmail.com>
2023-11-07 13:25:04 +0200

    Добавьте файлы проекта.

---
+++ MyStoreServer/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace MyStoreServer.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public short Quantity { get; set; }
+        public double Price { get; set; }
+        public string Photo { get; set; }
+
+    }
+}
